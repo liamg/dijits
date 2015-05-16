d24fb58da99b8a345b6f1ac6b464329195a1e5e8
Liam <liam.galvin@switchconcepts.com>
2015-05-16 01:19:21 +0100

    Removed unneeded grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,7 +65,6 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-qunit-istanbul');
-    grunt.loadNpmTasks('grunt-string-replace');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
